e698e296906d8749b6a101f924df74a316ee4da9
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-25 15:14:12 +0300

    created generateProducts.js file

---
+++ src/scripts/generateProducts.js
@@ -0,0 +1,13 @@
+import * as fs from 'fs/promises';
+import { createFakeProduct } from '../utils/createFakeProduct.js';
+import { somePath } from '../constants/products.js';
+
+const generateProducts = async (number) => {
+  const products = await fs.readFile(somePath, 'utf-8');
+  const parsedProducts = products ? JSON.parse(products) : [];
+  const newProductList = Array(number).fill(0).map(createFakeProduct);
+  const data = [...parsedProducts, ...newProductList];
+  await fs.writeFile(somePath, JSON.stringify(data, null, 2), 'utf-8');
+};
+
+generateProducts(3);
